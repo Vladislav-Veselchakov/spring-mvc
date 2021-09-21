ca76d7f0f0b1d230ae07518097b1739fe92d20ab
Vladislav A <validator1@yandex.ru>
2021-09-21 18:40:44 +0300

    tast complete 01

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -1,15 +1,20 @@
 package web.config;
 
+import model.Car;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -47,4 +52,5 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
+
 }
--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -9,16 +9,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+
 public class HelloController {
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
+		messages.add("I'm Spring MVC application ----");
+		messages.add("5.2.0 version by sep'3229 ");
 		model.addAttribute("messages", messages);
 		return "index";
 	}
-	
+
+// url: http://localhost:8080/manager/html
+//	Но перед этим пришлось в файле tomcat-users.xml вписать:
+//<role rolename="manager-gui"/>
+//<user username="tomcat" password="s3cret" roles="manager-gui"/>
+// on site
+// https://helpcontext.ru/questions/25999/intellij-tomcat-pokazyvaet-404-pri-zapuske
+
 }
